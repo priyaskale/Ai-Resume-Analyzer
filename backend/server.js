5a24f63cd36bdf27aec1a8cc4a45378bf850f457
priyaskale <priyaskale00947@gmail.com>
2026-08-09 18:26:56 +0530

    Fix CORS for production frontend

--- backend/server.js
+++ backend/server.js
@@ -26,11 +26,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 // -----------------------------
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  })
-);
+app.use(cors());
 
 app.use(express.json());
 
